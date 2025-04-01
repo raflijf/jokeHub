1a9ef35c38096f9c4ddc2cf398240d32d470a897
Rafli <raflijf7@gmail.com>
2025-04-01 21:02:19 +0700

    -

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import call_command 
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    call_command('collectstatic', interactive=False)
     execute_from_command_line(sys.argv)
 
 
