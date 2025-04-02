0a375d538da3f0e9920d8c2b1ea8e6932629dd0c
Rafli <raflijf7@gmail.com>
2025-04-02 14:35:10 +0700

    -

--- manage.py
+++ manage.py
@@ -15,8 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    if "runserver" in sys.argv or "migrate" in sys.argv:
-        call_command('collectstatic', interactive=False)
     execute_from_command_line(sys.argv)
 
 
